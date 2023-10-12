fbf9793ad1bd7dd39e4911047b1a986670ec0668
Nitya-Somani <nityasahajisomani@gmail.com>
2023-10-12 11:55:29 +0530

    YouTube-clone | Adding the collapsible bar feature

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,15 @@
 import React from 'react';
-
+import { useSelector } from 'react-redux';
 import Sidebar from "./SideBar";
 
 import { Outlet } from 'react-router-dom';
 import useOnlineStatus from '../utils/useOnlineStatus';
 
 
-
 const Body = () => {
   const netStatus = useOnlineStatus();
+  const theme = useSelector((store) => store.theme.isDarkTheme);
+
   if (netStatus === false) {
     return (
       <div className="flex items-center justify-center h-screen">
@@ -37,7 +38,7 @@ const Body = () => {
   
     
   return (
-    <div className='flex  '>
+    <div className={`flex ${theme ? "bg-gray-900" : "bg-white"} `}>
      <Sidebar/> 
       <Outlet/>
     
--- src/components/Watch.js
+++ src/components/Watch.js
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { useSearchParams } from "react-router-dom";
-
+import { closeMenu } from "../StoreSlices/appSlice";
 
 const Watch = () => {
   const paramsQuery = useSearchParams();
- 
 
-  
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(closeMenu());
+  }, []);
 
   return (
-    <div style={{margin:"5.5rem"}}>
+    <div style={{ margin: "5.5rem" }}>
       <iframe
         width="1000"
         height="500"
-        src={"https://www.youtube.com/embed/"+paramsQuery[0].get('v')}
+        src={"https://www.youtube.com/embed/" + paramsQuery[0].get("v")}
         title="YouTube video player"
-        frameborder="0"
+        frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowFullScreen
       ></iframe>
