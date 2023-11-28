09ebbdec98520a77a981550e15ebcd39b8bca474
Nitya-Somani <nityasahajisomani@gmail.com>
2023-11-28 23:57:46 +0530

    YouTubeClone | firebase auth

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,30 +1,77 @@
-import React, { useState } from 'react';
-
-
-
+import React, { useState, useRef } from "react";
+import checkValidateData from "../utils/validate";
 
 const Login = () => {
-    const [isSignIn,setIsSignIn] = useState(true);
-    const toggleSignIn = ()=>{
-        setIsSignIn(!isSignIn);
+  const [isSignIn, setIsSignIn] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
 
-    }
+  const toggleSignIn = () => {
+    setIsSignIn(!isSignIn);
+  };
+
+  const name = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
+  const handleBtnClick = () => {
+    const validateMsg = checkValidateData(
+      email.current.value,
+      password.current.value,
+      name.current.value
+    );
+    setErrorMessage(validateMsg);
+  };
   return (
-    <div className='h-screen w-full relative flex items-center justify-center'>
-      <img className='h-screen w-full absolute' src="https://img.freepik.com/premium-photo/youtube-template-3d-illustration_665513-18.jpg" alt="" />
-      <form className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-8 bg-transparent flex flex-col justify-center items-center text-white'>
-        <h4 className='font-bold text-3xl mb-6 text-left'>{isSignIn? " Sign In " :" Sign Up "}</h4>
+    <div className="h-screen w-full relative flex items-center justify-center">
+      <img
+        className="h-screen w-full absolute"
+        src="https://img.freepik.com/premium-photo/youtube-template-3d-illustration_665513-18.jpg"
+        alt=""
+      />
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className=" w-1/4 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-12 bg-black opacity-60  rounded-2xl flex flex-col justify-center items-center text-white"
+      >
+        <h4 className=" text-3xl mb-6 text-left">
+          {isSignIn ? " Sign In " : " Sign Up "}
+        </h4>
 
-        {isSignIn && <input type="text" placeholder='Full Name' className='p-3 mb-4 w-full bg-transparent border-b border-white focus:outline-none focus:border-red-500 text-white' />}
+        {isSignIn && (
+          <input
+            ref={name}
+            type="text"
+            placeholder="Full Name"
+            className="p-3 mb-4 w-full bg-transparent border-b border-white focus:outline-none focus:border-red-500 text-white"
+          />
+        )}
 
-
-        <input type="email" placeholder='Email Address' className='p-3 mb-4 w-full bg-transparent border-b border-white focus:outline-none focus:border-red-500 text-white' />
-        <input type="password" placeholder='Password' className='p-3 mb-6 w-full bg-transparent border-b border-white focus:outline-none focus:border-red-500 text-white' />
-        <button className='p-4 bg-red-500 w-full rounded-full font-bold hover:bg-red-600 transition-all'>{isSignIn? " Sign In" :" Sign Up"}</button>
-        <div className='mt-4 text-sm flex'>
-          <p className='text-blue-500 hover:underline'>Need help?</p>
-          <span className='mx-2'>|</span>
-          <p   onClick={toggleSignIn}  className='text-blue-500 hover:underline cursor-pointer'>{isSignIn? " Sign in now " :" Sign up now "}</p>
+        <input
+          ref={email}
+          type="email"
+          placeholder="Email Address"
+          className="p-3 mb-4 w-full bg-transparent border-b border-white focus:outline-none focus:border-red-500 text-white"
+        />
+        <input
+          ref={password}
+          type="password"
+          placeholder="Password"
+          className="p-3 mb-6 w-full bg-transparent border-b border-white focus:outline-none focus:border-red-500 text-white"
+        />
+        <p className="text-red-600 m-2 p-2 font-bold text-lg">{errorMessage}</p>
+        <button
+          onClick={handleBtnClick}
+          className="p-4 bg-red-500 w-full rounded-full font-bold hover:bg-red-600 transition-all"
+        >
+          {isSignIn ? " Sign In" : " Sign Up"}
+        </button>
+        <div className="mt-4 text-sm flex">
+          <p className="text-blue-500 hover:underline">Need help?</p>
+          <span className="mx-2">|</span>
+          <p
+            onClick={toggleSignIn}
+            className="text-blue-500 hover:underline cursor-pointer"
+          >
+            {isSignIn ? " Sign in now " : " Sign up now "}
+          </p>
         </div>
       </form>
     </div>
---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAiWUpI1UcB4AUFXjsE_An0wKjn9MwyYwI",
+  authDomain: "clone-335bc.firebaseapp.com",
+  projectId: "clone-335bc",
+  storageBucket: "clone-335bc.appspot.com",
+  messagingSenderId: "1060544593769",
+  appId: "1:1060544593769:web:9a62df10d2f1fa861987f8",
+  measurementId: "G-741NEPL5Y8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
