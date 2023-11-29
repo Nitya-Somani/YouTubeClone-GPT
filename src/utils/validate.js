1c2716f85489163f0caa655bc3856915bb3d9580
Nitya-Somani <nityasahajisomani@gmail.com>
2023-11-29 23:53:41 +0530

    Youtube Clone |Eslint fix

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,5 +1,5 @@
 const checkValidateData = (email, password,name)=>{
-const isEmailValid = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
+const isEmailValid = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
 
 const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
 const isNameValid =/(^[A-Za-z]{3,16})([ ]{0,1})([A-Za-z]{3,16})?([ ]{0,1})?([A-Za-z]{3,16})?([ ]{0,1})?([A-Za-z]{3,16})/.test(name);
