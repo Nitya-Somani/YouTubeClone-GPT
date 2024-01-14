cfb1092002b2a405e5c57f278230c8ff0e83b382
Nitya-Somani <nityasahajisomani@gmail.com>
2024-01-14 22:53:09 +0530

    reverting warnings

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,25 +1,20 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { useLocation, Outlet } from "react-router-dom";
+import { useLocation ,Outlet} from "react-router-dom";
 import { auth } from "../../utils/fireBaseAuth/firebase";
 import { onAuthStateChanged } from "firebase/auth";
 import { addUser, removeUser } from "../../StoreSlices/userSlice";
-import {
-  useOnlineStatus,
-  useFetchVideos,
-} from "../../utils/customHooks/hooksIndex";
-import {
-  Header,
-  SideBar,
-  ConnectionError,
-  BrowseHeader,
-  Shimmer,
-} from "../componentsIndex";
+import {useOnlineStatus,useFetchVideos} from '../../utils/customHooks/hooksIndex'
+import {Header,SideBar,ConnectionError, BrowseHeader, Shimmer} from '../componentsIndex'
+
 
 const Body = () => {
   const dispatch = useDispatch();
   const location = useLocation();
   const videos = useFetchVideos();
+  
+  
+
 
   useEffect(() => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -44,22 +39,22 @@ const Body = () => {
 
   const netStatus = useOnlineStatus();
   const isLoginPage = location.pathname === "/Login";
-  const isBrowsePage = location.pathname === "/browseTV";
-  const isErrorPage = location.pathname === "/oops";
+  const isBrowsePage =location.pathname === "/browseTV"
+  const isErrorPage =location.pathname === "/oops"
 
   const theme = useSelector((store) => store.theme.isDarkTheme);
 
   if (netStatus === false) {
     return <ConnectionError />;
   }
-  if (videos === null && !isBrowsePage) return <Shimmer />;
+  if (videos === null  && !isBrowsePage ) return <Shimmer />;
   return (
     <div className={`flex ${theme ? "bg-gray-900" : "bg-white"}`}>
-      {!isLoginPage && !isBrowsePage && !isErrorPage && <Header />}
-      {!isLoginPage && !isBrowsePage && !isErrorPage && <SideBar />}
-      {isBrowsePage ? <BrowseHeader /> : null}
-      {isErrorPage ? <BrowseHeader /> : null}
-
+       {!isLoginPage  && !isBrowsePage   && !isErrorPage && <Header />}
+      {!isLoginPage && !isBrowsePage   && !isErrorPage && <SideBar />} 
+      {isBrowsePage? <BrowseHeader/>:null}
+      {isErrorPage ? <BrowseHeader/>:null}
+      
       <Outlet />
     </div>
   );
