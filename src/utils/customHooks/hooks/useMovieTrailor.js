d35c875f08781adc4582e03d168941666f1b6dd0
Nitya-Somani <nityasahajisomani@gmail.com>
2024-01-14 23:33:05 +0530

    Ignorning warnings

--- src/utils/customHooks/hooks/useMovieTrailor.js
+++ src/utils/customHooks/hooks/useMovieTrailor.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import  { useEffect } from "react";
 import { options } from "../../../utils/constants/constants";
 import { useDispatch } from "react-redux";
 import {addTrailor} from '../../../StoreSlices/movieSlice'
