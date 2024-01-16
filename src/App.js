523194badff732af62b4966366d07fc4719a509f
Nitya-Somani <nityasahajisomani@gmail.com>
2024-01-16 14:21:39 +0530

    YouTube |error page dummy addition

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./index.css";
 import { Provider } from "react-redux";
 import store from '../src/store/store'
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import {Body, Login,  Watch, MainContainer, Error ,BrowseTv} from './components/componentsIndex';
+import {Body, Login,  Watch, MainContainer, Error ,BrowseTv,GPTView} from './components/componentsIndex';
 
 
 function App() {
@@ -37,19 +37,20 @@ const appRouter = createBrowserRouter([
         element: <Watch />,
         errorElement: <Error />,
       },
-      {
-        path: "/oops",
-        element: <Error />,
-        errorElement: <Error />,
-      },
+   
       {
         path: "/browseTV",
         element: <BrowseTv />,
         errorElement: <Error />,
       },
       {
-        path: "/GPT",
+        path: "/Oops",
         element: <Error/>,//<GPTView/>,
+        errorElement: <Error />,
+      },
+      {
+        path: "/GPT",
+        element: <GPTView/>,
         errorElement: <Error />,
       }
   
